9051606292318d1ad934d66031f34803beec5100
Uros <gomilseku@gmail.com>
2025-05-07 14:12:29 +0200

    avoid hydration warning for graph components

--- insights-app/app/routes/dashboard.tsx
+++ insights-app/app/routes/dashboard.tsx
@@ -3,7 +3,7 @@ import BarChartClient from "@/components/consumption-bar-graph";
 import ConsumptionLineGraph from "@/components/consumption-line-graph";
 import type { Route } from "./+types/dashboard";
 import { getReadings } from "@/services/api";
-
+import React from "react";
 export async function loader() {
   const readings = await getReadings();
 
@@ -13,15 +13,21 @@ export async function loader() {
 export default function Dashboard({
   loaderData: { readings },
 }: Route.ComponentProps) {
+  const [isClient, setIsClient] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <>
       <h1 className="text-2xl font-bold">Energy consumption insights</h1>
       <div className="flex flex-col lg:flex-row gap-4 w-full">
         <div className="w-full lg:w-1/2">
-          {/* <ConsumptionLineGraph data={readings} /> */}
+          {isClient && <ConsumptionLineGraph data={readings} />}
         </div>
         <div className="w-full lg:w-1/2">
-          <BarChartClient data={readings} />
+          {isClient && <BarChartClient data={readings} />}
         </div>
       </div>
     </>
